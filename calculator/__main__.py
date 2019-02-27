b3a65cb299ff8b1371fc71d107bbb8cdc09a15b5
Rene Caspart <rene.caspart@cern.ch>
2019-02-27 18:03:59 +0100

    Adaptions to pep8

--- calculator/__main__.py
+++ calculator/__main__.py
@@ -2,22 +2,22 @@ import argparse
 import parse_input
 
 def main():
-    CLI = argparse.ArgumentParser('')
-    CLI.add_argument(
+    cli = argparse.ArgumentParser('')
+    cli.add_argument(
         '-v', '--verbose',
         dest="verbose",
         help="Enable verbose output",
         default=False,
         action="store_true"
     )
-    CLI.add_argument(
+    cli.add_argument(
         'operations',
         metavar="N",
         type=str,
         nargs="+",
         help="Operation to be performed"
     )
-    options = CLI.parse_args()
+    options = cli.parse_args()
     return parse_input.parse(options.operations, options.verbose)
 
 if __name__ == "__main__":
--- calculator/parse_input.py
+++ calculator/parse_input.py
@@ -1,33 +1,33 @@
 from calculator import operations
 
+import operations
 
-def parse(inputs, verbose=False):
+
+def parse(inputs):
     """Parse the input provided
-    :param: input: input parameters for the calculation
-    :type input: sorted iterable of strings
-    :param: verbose: Verbosity setting
-    :type verbose: boolean
+    :param: inputs: input parameters for the calculation
+    :type inputs: sorted iterable of strings
     :rtype float"""
     result = None
     operation = None
     operation_map = {
         "+": operations.add,
         "-": operations.subtract,
-        "*": operations.multiply,
+        "x": operations.multiply,
         "/": operations.devide
     }
 
-    for input in inputs:
-        if input in operation_map:
-            operation = operation_map[input]
+    for ip in inputs:
+        if ip in operation_map:
+            operation = operation_map[ip]
             if result == None:
                 raise(Exception("Provided operations without arguments"))
         else:
             try:
-                tmp_input = float(input)
+                tmp_ip = float(ip)
             except ValueError as e:
                 raise (Exception(
-                    "Provided input not understood. Either the operation is not supported or the input is malformed %s" % input))
+                    "Provided input not understood. Either the operation is not supported or the input is malformed %s" % ip))
             if result == None:
                 result = tmp_input
             else:
